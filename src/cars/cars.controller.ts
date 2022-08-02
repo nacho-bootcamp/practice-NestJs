5b3de97a692cf115b88b055621262d93c5ec4164
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-08-02 11:19:53 -0300

    feat:dependency injection

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 //los controladores escuchan la peticiones y dan una respuesta
 @Controller('cars')
 export class CarsController {
-  private cars = ['Ferrari', 'Toyota', 'Jeep'];
+  constructor(private readonly CarsService: CarsService) {}
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.CarsService.findAll;
   }
   @Get(':id')
-  getCarById(@Param('id') id) {
+  getCarById(@Param('id') id: string) {
     //para obtener el parametro por el id se usa eñ @param("id")
     console.log({ id });
-    return this.cars[id];
+    return this.CarsService.findOneById(Number(id));
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -19,4 +19,13 @@ export class CarsService {
       model: 2018,
     },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    return car;
+  }
 }
